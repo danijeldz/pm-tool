f65549b36b63d0fdfbb4dc0c9b3c9a51c0e2ed74
danijeldz <danijeldzankic@hotmail.com>
2020-03-25 22:42:45 +0100

    vutify text and buttons styling with props and classes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <h2>Text classes</h2>
+    <p
+      class="red white--text"
+    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus asperiores, possimus pariatur neque, libero eligendi non fugit earum ipsam est doloremque. Quia in accusantium autem necessitatibus incidunt corrupti cumque ipsam.</p>
+    <p
+      class="pink lighten-4 red--text text--darken-4"
+    >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos non at sit necessitatibus adipisci, quis accusamus, ut sequi, qui harum facere quam veritatis exercitationem numquam temporibus excepturi. Architecto, ipsam repellendus.</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller display</h4>
+    <p class="headline">THis is a headline</p>
+    <p class="subheading font-weight-bold">This is a subheading</p>
+    <p class="caption">THis is a caption</p>
+
+    <h2>Button classes and props</h2>
+    <v-btn class="pink white--text">Click me</v-btn>
+    <v-btn depressed color="pink">Click me - depressed</v-btn>
+    <v-btn text color="pink">click me - flat</v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon left>mdi-email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed small class="pink white--text">
+      <v-icon small left>mdi-email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed large class="pink white--text">
+      <span>email me</span>
+      <v-icon large right>mdi-email</v-icon>
+    </v-btn>
+
+    <v-btn depressed fab small dark color="purple">
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home"
+};
 </script>
