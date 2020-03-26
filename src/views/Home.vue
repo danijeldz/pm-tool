41e3b25b5ee755dccd25ecdc811f086473b49d87
danijeldz <danijeldzankic@hotmail.com>
2020-03-26 15:36:45 +0100

    tested breakpoints and visibility classes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -37,6 +37,16 @@
     <v-btn depressed fab small dark color="purple">
       <v-icon>mdi-heart</v-icon>
     </v-btn>
+
+    <h2>Breakpoints and visibility</h2>
+    <p>button hidden for medium and smaller screens</p>
+    <v-btn class="hidden-md-and-down">Click me</v-btn>
+
+    <p>button hidden for medium and bigger screens</p>
+    <v-btn class="hidden-md-and-up">Click me</v-btn>
+
+    <p>button hidden for small screens only</p>
+    <v-btn class="hidden-sm-only">Click me</v-btn>
   </div>
 </template>
 
