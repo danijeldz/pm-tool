39a4052fdaf665189820cd86b82f1e201761a4d1
danijeldz <danijeldzankic@hotmail.com>
2020-03-26 19:13:42 +0100

    created routes in the toolbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,30 @@
 <template>
   <nav>
-    <v-toolbar flat app>
+    <v-app-bar text app>
+      <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span>
         <span>Ninja</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn flat color="grey">
+      <v-btn text color="grey">
         <span>Sign Out</span>
-        <v-icon right>mdi-exit-to-app</v-icon>
+        <v-icon right>exit_to_app</v-icon>
       </v-btn>
-    </v-toolbar>
+    </v-app-bar>
+
+    <v-navigation-drawer temporary app class="primary" v-model="drawer">
+      <v-list>
+        <v-list-item v-for="(link,index) in links" :key="index" router :to="link.route">
+          <v-list-item-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </nav>
 </template>
 
@@ -18,7 +32,14 @@
 export default {
   name: "Navbar",
   data() {
-    return {};
+    return {
+      drawer: false,
+      links: [
+        { icon: "dashboard", text: "Dashboards", route: "/" },
+        { icon: "folder", text: "My Projects", route: "/projects" },
+        { icon: "person", text: "Team", route: "/team" }
+      ]
+    };
   }
 };
 </script>
