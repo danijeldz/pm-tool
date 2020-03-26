71fa651950659ee458ae3c751b1939387a647783
danijeldz <danijeldzankic@hotmail.com>
2020-03-26 15:53:37 +0100

    created template for navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <v-app>
     <v-content>
+      <Navbar />
       <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar";
+
 export default {
   name: "App",
-
+  components: {
+    Navbar
+  },
   data: () => ({
     //
   })
---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="navabar">
+    <h2>Navbar</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
