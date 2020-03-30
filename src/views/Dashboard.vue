e2c55c12fb19a563d4224c1dbf42497c3acb8977
danijeldz <danijeldzankic@hotmail.com>
2020-03-30 14:31:03 +0200

    created grid with projects and dummy data

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,29 +1,32 @@
 <template>
   <div class="dashboard">
-    <h1 class="subheding grey--text">Dashboard</h1>
+    <h1 class="subheading grey--text">Dashboard</h1>
     <v-container class="my-5">
-      <v-layout row wrap>
-        <v-flex xs12 md6>
-          <v-btn outline block class="primary">1</v-btn>
-        </v-flex>
-        <v-flex xs4 md2>
-          <v-btn outline block class="primary">2</v-btn>
-        </v-flex>
-        <v-flex xs4 md2>
-          <v-btn outline block class="primary">2</v-btn>
-        </v-flex>
-        <v-flex xs4 md2>
-          <v-btn outline block class="primary">2</v-btn>
-        </v-flex>
-      </v-layout>
-      <v-layout row wrap justify-center>
-        <v-flex xs4 md3>
-          <v-btn outline block class="success">1</v-btn>
-        </v-flex>
-        <v-flex xs4 md3>
-          <v-btn outline block class="success">1</v-btn>
-        </v-flex>
-      </v-layout>
+      <v-card flat class="pl-4" v-for="(project, index) in projects" :key="index">
+        <v-layout row wrap :class="`project pa-3 ${project.status}`">
+          <v-flex xs12 md6>
+            <div class="caption grey--text">Project title:</div>
+            <div>{{ project.title }}</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Person</div>
+            <div>{{ project.person }}</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Due date</div>
+            <div>{{ project.due }}</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div right class="right">
+              <v-chip
+                small
+                :class="`${project.status} v-chip--active white--text caption my-2`"
+              >{{ project.status }}</v-chip>
+            </div>
+          </v-flex>
+        </v-layout>
+        <v-divider></v-divider>
+      </v-card>
     </v-container>
   </div>
 </template>
@@ -32,10 +35,63 @@
 export default {
   name: "Dashboard",
   data() {
-    return {};
+    return {
+      projects: [
+        {
+          title: "Design a new website",
+          person: "The Net Ninja",
+          due: "1st Jun 2020",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+        {
+          title: "Code up the homepage",
+          person: "Chun Li",
+          due: "10th Jan 2020",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+        {
+          title: "Design video thumbnails",
+          person: "Ryu",
+          due: "20th Dec 2019",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+        {
+          title: "Create a community forum",
+          person: "Gouken",
+          due: "20th Oct 2019",
+          status: "overdue",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        }
+      ]
+    };
   }
 };
 </script>
 
 <style>
+.project.complete {
+  border-left: 4px solid #3cd1c2;
+}
+.project.ongoing {
+  border-left: 4px solid orange;
+}
+.project.overdue {
+  border-left: 4px solid tomato;
+}
+.v-chip.complete {
+  background: #3cd1c2;
+}
+.v-chip.ongoing {
+  background: #ffaa2c;
+}
+.v-chip.overdue {
+  background: #f83e70;
+}
 </style>
