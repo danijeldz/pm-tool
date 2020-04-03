ac7e822771a5bb8f688374136ea85e4fc97aec23
danijeldz <danijeldzankic@hotmail.com>
2020-04-03 15:26:02 +0200

    added form validation

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -8,9 +8,9 @@
         <h2>Add a new project</h2>
       </v-card-title>
       <v-card-text>
-        <v-form class="px-3">
-          <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
-          <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+        <v-form class="px-3" ref="form">
+          <v-text-field label="Title" v-model="title" prepend-icon="folder" :rules="inputRules"></v-text-field>
+          <v-textarea label="Information" v-model="content" prepend-icon="edit" :rules="inputRules"></v-textarea>
           <v-menu max-width="290">
             <template v-slot:activator="{on}">
               <v-text-field
@@ -18,6 +18,7 @@
                 label="Due date:"
                 prepend-icon="date_range"
                 v-on="on"
+                :rules="inputRules"
               ></v-text-field>
             </template>
             <v-date-picker v-model="due"></v-date-picker>
@@ -38,12 +39,17 @@ export default {
     return {
       title: "",
       content: "",
-      due: null
+      due: null,
+      inputRules: [
+        v => v.length >= 3 || "Minimum length of the text is 3 characters."
+      ]
     };
   },
   methods: {
     submit() {
-      console.log(this.title, this.content);
+      if (this.$refs.form.validate()) {
+        console.log(this.title, this.content);
+      }
     }
   },
   computed: {
