d95dfaed21ee6022e2833ba88ceab311712f9a62
danijeldz <danijeldzankic@hotmail.com>
2020-04-03 15:06:50 +0200

    added date picker to the form

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -11,29 +11,44 @@
         <v-form class="px-3">
           <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
           <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+          <v-menu max-width="290">
+            <template v-slot:activator="{on}">
+              <v-text-field
+                :value="formattedDate"
+                label="Due date:"
+                prepend-icon="date_range"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker v-model="due"></v-date-picker>
+          </v-menu>
           <v-btn text class="success mx-o mt-3" @click="submit">Add project</v-btn>
         </v-form>
       </v-card-text>
-
-      <!-- <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn text color="primary" @click="dialog = false">Add project</v-btn>
-      </v-card-actions>-->
     </v-card>
   </v-dialog>
 </template>
 
 <script>
+//import format from "date-fns/format";
+import moment from "moment";
+
 export default {
   data() {
     return {
       title: "",
-      content: ""
+      content: "",
+      due: null
     };
   },
   methods: {
     submit() {
       console.log(this.title, this.content);
+    }
+  },
+  computed: {
+    formattedDate() {
+      return this.due ? moment(this.due).format("Do MMMM YYYY") : "";
     }
   }
 };
