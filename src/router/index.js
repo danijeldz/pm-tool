72d7c5195ab78639775ef86711fadc1300f580bc
danijeldz <danijeldzankic@hotmail.com>
2020-04-04 22:38:34 +0200

    created template for read/edit single project

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router"
 import Dashboard from "../views/Dashboard.vue"
 import Project from "../views/Projects.vue"
 import Team from "../views/Team.vue"
+import SingleProject from "../views/SingleProject"
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,11 @@ const routes = [
     path: "/team",
     name: "team",
     component: Team
+  },
+  {
+    path: "/project/:project_slug",
+    name: "SingleProject",
+    component: SingleProject
   }
 ]
 
---
+++ src/views/SingleProject.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="single-project">
+    <h1 class="subheading grey--text">Single Project {{ this.$route.params.project_slug}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SingleProject",
+  data() {
+    return {};
+  }
+};
+</script>
+
